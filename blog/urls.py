83492bfda123191b9f1e83094994f93f36889c17
auralcat <ashleyzt@hi2.in>
2017-03-20 13:56:24 -0300

    Fixed bug and finished implementing forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,4 @@
 """URLs for the site are defined in this file."""
-
 from django.conf.urls import url
 from . import views
 
@@ -7,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9])+/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
